146c003d2c2dceddfd804d18bed2dc3a0197c710
zhouwenchen <761075116@qq.com>
2020-08-13 20:59:10 +0800

    二分查找的四种变形题型

---
+++ leetcode-zwc/src/main/java/com/sh/study/search/BinarySearch.java
@@ -0,0 +1,65 @@
+package com.sh.study.search;
+
+/**
+ * 二分查找,查找指定数据，并返回目标数据在数据中的位置，-1 表示数组中没有指定的数据
+ *
+ * @Author zhouwenchen
+ * @Data 2020/8/13/15
+ **/
+public class BinarySearch {
+
+    /**
+     * 二分查找 指定目标的数据
+     * 使用 while 循环实现
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int binarySearch(int[] nums, int target) {
+        if (nums == null) {
+            return -1;
+        }
+        int low = 0;
+        int high = nums.length - 1;
+        while (low < high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = high - 1;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 使用递归实现
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int binarySearch0(int[] nums, int start, int end, int target) {
+        if (nums == null || start > end) {
+            return -1;
+        }
+        int mid = (start + end) >> 1;
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] > target) {
+            return binarySearch0(nums, start, mid - 1, target);
+        } else {
+            return binarySearch0(nums, mid + 1, end, target);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 5, 7, 8, 11, 19, 23, 27, 33, 45, 55, 67, 98};
+        int target = 27;
+        System.out.println("location is " + binarySearch(nums, target));
+        System.out.println("location is " +  binarySearch0(nums, 0, nums.length - 1, target));
+    }
+}
---
+++ leetcode-zwc/src/main/java/com/sh/study/search/BinarySearchDemo.java
@@ -0,0 +1,198 @@
+package com.sh.study.search;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 二分查找的几个变形问题
+ * <p>
+ * 1:查找第一个值等于给定值的元素
+ * 2:查找最后一个值等于给定值的元素
+ * 3:查找第一个大于等于给定值的元素
+ * 4:查找最后一个小于等于给定值的元素
+ *
+ * @Author zhouwenchen
+ * @Data 2020/8/13/16
+ **/
+public class BinarySearchDemo {
+
+    /**
+     * 1:查找第一个值等于给定值的元素
+     * 思路：先根据二分查找，当 mid 满足 nums[mid] == target,需要再向前判断一位  nums[mid -1] 是否等于  target
+     * 如果不等于，就直接返回mid
+     * 如果等于，需要继续使用二分查找
+     *
+     * @return
+     */
+    public static int binarySearch1(int[] nums, int target) {
+        if (nums == null) {
+            return -1;
+        }
+        int start = 0;
+        int end = nums.length - 1;
+
+        // 判断第一个是否等于 target，如果等于，直接返回
+        if (nums[start] == target) {
+            return start;
+        }
+        while (start <= end) {
+            int mid = start + (((end - start)) >> 1);
+            if (nums[mid] == target) {
+                // 需要判断前一位是否也等于 target,不等于直接返回
+                if (nums[mid - 1] != target) {
+                    return mid;
+                }
+                // nums[mid -1] == target
+                end = mid - 1;
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 2:查找最后一个值等于给定值的元素
+     * 思路：先根据二分查找，当 mid 满足 nums[mid] == target,需要再向后判断一位  nums[mid + 1] 是否等于  target
+     * 如果不等于，就直接返回mid
+     * 如果等于，需要继续使用二分查找
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int binarySearch2(int[] nums, int target) {
+        if (nums == null) {
+            return -1;
+        }
+        int start = 0;
+        int end = nums.length - 1;
+
+        // 判断最后一个是否等于 target，如果等于，直接返回
+        if (nums[end] == target) {
+            return end;
+        }
+        while (start <= end) {
+            int mid = start + (((end - start)) >> 1);
+            if (nums[mid] == target) {
+                // 需要判断后一位是否也等于 target,不等于直接返回
+                if (nums[mid + 1] != target) {
+                    return mid;
+                }
+                // nums[mid + 1] == target
+                start = mid + 1;
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 3:查找第一个大于等于给定值的元素
+     * 这题思路同第一个  `查找第一个值等于给定值的元素`
+     * <p>
+     * 思路：先根据二分查找，当 mid 满足 nums[mid] == target,需要再向前判断一位  nums[mid -1] 是否等于  target
+     * 如果不等于，就直接返回mid
+     * 如果等于，需要继续使用二分查找
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int binarySearch3(int[] nums, int target) {
+        if (nums == null) {
+            return -1;
+        }
+        int start = 0;
+        int end = nums.length - 1;
+
+        // 判断第一个是否大于等于 target，如果满足，直接返回
+        if (nums[start] >= target) {
+            return start;
+        }
+        if(nums[end] < target){
+            return -1;
+        }
+        while (start <= end) {
+            int mid = start + (((end - start)) >> 1);
+            if (nums[mid] == target) {
+                // 需要判断前一位是否也大于等于 target,不满足 直接返回
+                if (nums[mid - 1] != target) {
+                    return mid;
+                }
+                // nums[mid + 1] == target
+                end = mid - 1;
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        if (end < start && nums[end] < target && nums[start] > target) {
+            return start;
+        }
+        return -1;
+    }
+
+    /**
+     * 4:查找最后一个小于等于给定值的元素
+     * 思路同 `查找最后一个值等于给定值的元素`
+     * <p>
+     * 思路：先根据二分查找，当 mid 满足 nums[mid] == target,需要再向后判断一位  nums[mid + 1] 小于 等于  target
+     * 如果不等于，就直接返回mid
+     * 如果等于，需要继续使用二分查找
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int binarySearch4(int[] nums, int target) {
+        if (nums == null) {
+            return -1;
+        }
+        int start = 0;
+        int end = nums.length - 1;
+
+        // 判断最后一个是否大于等于 target，如果满足，直接返回
+        if (nums[end] <= target) {
+            return end;
+        }
+        if(nums[start] > target){
+            return -1;
+        }
+        while (start <= end) {
+            int mid = start + (((end - start)) >> 1);
+            if (nums[mid] == target) {
+                // 需要判断后一位是否也大于等于 target,不满足 直接返回
+                if (nums[mid + 1] != target) {
+                    return mid;
+                }
+                // nums[mid + 1] == target
+                start = mid + 1;
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        if(end < start && nums[end] < target && nums[start] > target){
+            return end;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 1, 2, 2, 2, 3, 4, 5, 5, 6, 6, 6, 9, 11, 15,15};
+        // target 的测试取值，要包含多种情况 0,1,2,7,15,17 这几种情况
+        int target = 17;
+        System.out.println("查找第一个值等于给定值的元素:" + binarySearch1(nums, target));
+        System.out.println("查找最后一个值等于给定值的元素:" + binarySearch2(nums, target));
+        System.out.println("查找第一个大于等于给定值的元素:" + binarySearch3(nums, target));
+        System.out.println("查找最后一个小于等于给定值的元素:" + binarySearch4(nums, target));
+    }
+}
