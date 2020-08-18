a6b4745e8204fa1fa88217932ce0108348396ed2
761075116@qq.com <761075116@qq.com>
2020-08-18 08:48:55 +0800

    堆的删除操作

--- leetcode-zwc/src/main/java/com/sh/study/heap/Heapsort.java
+++ leetcode-zwc/src/main/java/com/sh/study/heap/Heapsort.java
@@ -62,21 +62,63 @@ public class Heapsort {
         nums[j] = temp;
     }
 
+    /**
+     * 删除最大堆
+     * 删除堆顶的元素，让堆的最后一个节点替换，然后堆化，使其满足堆的特点
+     */
+    public void removeHeapMax(){
+        if(count == 0){
+            return;
+        }
+        // 堆的最后一个元素替换堆顶元素
+        values[1] = values[count];
+        --count;
+        heapify(values,count,1);
+    }
+
+    /**
+     * 自上而下的堆化过程
+     */
+    private void heapify(int[] values, int count, int i) {
+        while (true){
+            int maxPos = i;
+            // 左子节点
+            if(i * 2 <= count && values[i] < values[i*2]){
+                maxPos = i * 2;
+            }
+            // 右子节点
+            if(i * 2 + 1 <= count && values[maxPos] < values[i*2 + 1]){
+                maxPos = i*2+1;
+            }
+            // 父节点不比左子节点和右子节点大
+            if(maxPos == i){
+                break;
+            }
+            swap(values, i, maxPos);
+            i = maxPos;
+        }
+    }
+
     public static void main(String[] args) {
         /**
          * 插入操作
          *
-         * 0,33,17,21,16,13,15,9,5,6,7,8,1,2,0,0,0,0,0,0,0,
-         * 0,33,17,22,16,13,15,21,5,6,7,8,1,2,9,0,0,0,0,0,0,
+         * 原：0,33,17,21,16,13,15,9,5,6,7,8,1,2,0,0,0,0,0,0,0,
+         * 插：0,33,17,22,16,13,15,21,5,6,7,8,1,2,9,0,0,0,0,0,0,
+         * 删：
          */
         Heapsort heap = new Heapsort(20);
         AtomicInteger i = new AtomicInteger();
-        int[] nums = new int[]{0, 33, 17, 21, 16, 13, 15, 9, 5, 6, 7, 8, 1, 2};
+//        int[] nums = new int[]{0, 33, 17, 21, 16, 13, 15, 9, 5, 6, 7, 8, 1, 2};
+        int[] nums = new int[]{0, 33, 27, 21, 16, 13, 15, 9, 5, 6, 7, 8, 1, 2,12};
         Arrays.stream(nums).forEach(value -> heap.values[i.getAndIncrement()] = value);
         heap.count = nums.length-1;
         Arrays.stream(heap.values).forEach(o -> System.out.print(o + ","));
         System.out.println();
-        heap.insertHeapSort(22);
+//        heap.insertHeapSort(22);
+        heap.removeHeapMax();
         Arrays.stream(heap.values).forEach(o -> System.out.print(o + ","));
+
+
     }
 }
