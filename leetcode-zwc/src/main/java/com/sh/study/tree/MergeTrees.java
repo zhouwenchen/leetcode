897cbb1cd6d35c52bef89d8f16bdbdc2afb327ac
zhouwenchen <761075116@qq.com>
2020-09-23 14:11:36 +0800

    合并二叉树

--- leetcode-zwc/src/main/java/com/sh/study/tree/MergeTrees.java
+++ leetcode-zwc/src/main/java/com/sh/study/tree/MergeTrees.java
@@ -1,9 +1,50 @@
 package com.sh.study.tree;
 
-/*
+import com.sh.study.node.TreeNode;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * 617. 合并二叉树
+ * 给定两个二叉树，想象当你将它们中的一个覆盖到另一个上时，两个二叉树的一些节点便会重叠。
+ * 你需要将他们合并为一个新的二叉树。合并的规则是如果两个节点重叠，那么将他们的值相加作为节点合并后的新值，否则不为 NULL 的节点将直接作为新二叉树的节点。
+ *
+ * 示例 1:
+ *
+ * 输入:
+ * 	Tree 1                     Tree 2
+ *           1                         2
+ *          / \                       / \
+ *         3   2                     1   3
+ *        /                           \   \
+ *       5                             4   7
+ * 输出:
+ * 合并后的树:
+ * 	     3
+ * 	    / \
+ * 	   4   5
+ * 	  / \   \
+ * 	 5   4   7
+ *
  * @Author zhouwenchen
  * @Date  2020-09-23
  **/
 public class MergeTrees {
+    public static TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if(t1==null){
+            return t2;
+        }
+        if(t2== null){
+            return t1;
+        }
+        TreeNode merged = new TreeNode(t1.val + t2.val);
+        merged.left = mergeTrees(t1.left , t2.left);
+        merged.right = mergeTrees(t1.right,t2.right);
+        return merged;
+    }
+
+    public static void main(String[] args) {
+//        mergeTrees();
+    }
 }
