6d80236c30c70f2d0d1e8ba5e1c717256162181f
zhouwenchen <761075116@qq.com>
2020-10-13 19:56:48 +0800

    实现Trie

---
+++ leetcode-zwc/src/main/java/com/sh/study/exercise/nine/Trie.java
@@ -0,0 +1,58 @@
+package com.sh.study.exercise.nine;
+
+/**
+ * 208. 实现 Trie (前缀树)
+ * 实现一个 Trie (前缀树)，包含 insert, search, 和 startsWith 这三个操作。
+ *
+ * 示例:
+ *
+ * Trie trie = new Trie();
+ *
+ * trie.insert("apple");
+ * trie.search("apple");   // 返回 true
+ * trie.search("app");     // 返回 false
+ * trie.startsWith("app"); // 返回 true
+ * trie.insert("app");
+ * trie.search("app");     // 返回 true
+ * 说明:
+ *
+ * 你可以假设所有的输入都是由小写字母 a-z 构成的。
+ * 保证所有输入均为非空字符串。
+ *
+ * @Author zhouwenchen
+ * @Date  2020-10-13
+ **/
+public class Trie {
+
+    /**
+     * Your Trie object will be instantiated and called as such:
+     * Trie obj = new Trie();
+     * obj.insert(word);
+     * boolean param_2 = obj.search(word);
+     * boolean param_3 = obj.startsWith(prefix);
+     */
+
+    /** Initialize your data structure here. */
+    public Trie() {
+
+    }
+
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+
+    }
+
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+        return true;
+    }
+
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String prefix) {
+        return true;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
