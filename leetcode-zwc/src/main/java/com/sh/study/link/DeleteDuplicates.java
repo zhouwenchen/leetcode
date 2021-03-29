c74ba714fe9ec09a4041df8946d03fd15bf071b7
zhouwenchen <761075116@qq.com>
2021-03-29 14:29:01 +0800

    82. 删除排序链表中的重复元素 II

--- leetcode-zwc/src/main/java/com/sh/study/link/DeleteDuplicates.java
+++ leetcode-zwc/src/main/java/com/sh/study/link/DeleteDuplicates.java
@@ -83,10 +83,54 @@ public class DeleteDuplicates {
         return dumy.next;
     }
 
+    /**
+     * 82. 删除排序链表中的重复元素 II
+     * 给定一个排序链表，删除所有含有重复数字的节点，只保留原始链表中 没有重复出现 的数字。
+     * 示例 1:
+     * 输入: 1->2->3->3->4->4->5
+     * 输出: 1->2->5
+     * 示例 2:
+     * 输入: 1->1->1->2->3
+     * 输出: 2->3
+     *
+     * 思路，统计重复的元素，直接跳过
+     * @date 20210325
+     * @param head
+     * @return
+     */
+    public static ListNode deleteDuplicates3(ListNode head) {
+        if(head == null){
+            return null;
+        }
+        ListNode dumy = new ListNode(-1);
+        dumy.next = head;
+        ListNode pre = dumy;
+        ListNode cur = dumy.next;
+
+        while (cur != null){
+            int count = 0;
+            ListNode differNode = cur;
+            while (differNode.next != null && differNode.val == differNode.next.val){
+                count++;
+                differNode = differNode.next;
+            }
+            if(count > 0){
+                pre.next = differNode.next;
+            }else {
+                pre = cur;
+            }
+            cur = differNode.next;
+        }
+
+        return dumy.next;
+    }
+
+
     public static void main(String[] args) {
-//        ListNode result = deleteDuplicates2(NodeUtil.createListNodeByArr(new int[]{1, 1, 2, 3, 3,3}));
-        ListNode result = deleteDuplicates2(NodeUtil.createListNodeByArr(new int[]{1, 1, 1, 2, 3}));
-//        ListNode result = deleteDuplicates2(NodeUtil.createListNodeByArr(new int[]{1, 1, 2, 3, 3,4,4,5}));
+//        ListNode result = deleteDuplicates3(NodeUtil.createListNodeByArr(new int[]{1, 1, 2, 3, 3,3}));
+//        ListNode result = deleteDuplicates3(NodeUtil.createListNodeByArr(new int[]{1, 1, 1, 2, 3}));  //2->3->null
+        ListNode result = deleteDuplicates3(NodeUtil.createListNodeByArr(new int[]{1, 1, 2, 3, 3,4,4,5}));
+//        ListNode result = deleteDuplicates3(NodeUtil.createListNodeByArr(new int[]{1,2,3,3,4,4,5}));
         NodeUtil.printListNode(result);
     }
 }
