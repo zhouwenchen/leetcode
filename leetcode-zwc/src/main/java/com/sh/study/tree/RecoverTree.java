c58807e50b697fec6f60c87a581d010efb506d73
zhouwenchen <761075116@qq.com>
2021-04-06 10:33:22 +0800

    99. 恢复二叉搜索树

---
+++ leetcode-zwc/src/main/java/com/sh/study/tree/RecoverTree.java
@@ -0,0 +1,37 @@
+package com.sh.study.tree;
+
+import com.sh.study.node.TreeNode;
+
+/**
+ * 99. 恢复二叉搜索树
+ * 给你二叉搜索树的根节点 root ，该树中的两个节点被错误地交换。请在不改变其结构的情况下，恢复这棵树。
+ *
+ * 进阶：使用 O(n) 空间复杂度的解法很容易实现。你能想出一个只使用常数空间的解决方案吗？
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [1,3,null,null,2]
+ * 输出：[3,1,null,null,2]
+ * 解释：3 不能是 1 左孩子，因为 3 > 1 。交换 1 和 3 使二叉搜索树有效。
+ * 示例 2：
+ *
+ *
+ * 输入：root = [3,1,4,null,null,2]
+ * 输出：[2,1,4,null,null,3]
+ * 解释：2 不能在 3 的右子树中，因为 2 < 3 。交换 2 和 3 使二叉搜索树有效。
+ *
+ * @author zhouwenchen
+ * @date 2021/3/30 16:40
+ **/
+public class RecoverTree {
+    public static void recoverTree(TreeNode root) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
