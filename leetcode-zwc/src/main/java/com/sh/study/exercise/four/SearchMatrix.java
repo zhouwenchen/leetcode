5421036dc0ac717c28ad3812d38709eb2c5cca65
zhouwenchen <761075116@qq.com>
2021-04-06 10:34:09 +0800

    74. 搜索二维矩阵

--- leetcode-zwc/src/main/java/com/sh/study/exercise/four/SearchMatrix.java
+++ leetcode-zwc/src/main/java/com/sh/study/exercise/four/SearchMatrix.java
@@ -57,17 +57,74 @@ public class SearchMatrix {
         return false;
     }
 
+    /**
+     * 20210330
+     * 遍历方式，从右上到左下
+     * @param matrix
+     * @param target
+     * @return
+     */
+    public static boolean searchMatrix1(int[][] matrix, int target) {
+        // 验证参数的有效性
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0){
+            return false;
+        }
+        int i = 0;
+        int j = matrix[0].length - 1 ;
+        while (i < matrix.length && j >= 0){
+            if(matrix[i][j] == target){
+                return true;
+            }else if(matrix[i][j] > target){
+                j--;
+            }else {
+                i++;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 从左下到右上
+     * @param matrix
+     * @param target
+     * @return
+     */
+    public static boolean searchMatrix2(int[][] matrix, int target) {
+        // 验证参数
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0){
+            return false;
+        }
+
+        int i = matrix.length-1;
+        int j = 0;
+        while (i >= 0 && j <= matrix[0].length-1){
+            if(matrix[i][j] == target){
+                return true;
+            } else if(matrix[i][j] > target){
+                i--;
+            }else {
+                j++;
+            }
+
+        }
+        return false;
+    }
+
+
     public static void main(String[] args) {
 //        int[][] matrix = new int[][]{
 //                {1,  3,  5,  7},
 //                {10, 11, 16, 20},
 //                {23, 30, 34, 50},
 //        };
-//        int target = 23;
+//        int target = 3;
 
-        int[][] matrix = new int[][]{
-        };
-        int target = 0;
-        System.out.println(searchMatrix(matrix, target));
+//        int[][] matrix = new int[][]{
+//        };
+//        int target = 0;
+
+        int[][] matrix = new int[][]{{1}};
+        int target = 1;
+        System.out.println(searchMatrix2(matrix, target));
     }
 }
