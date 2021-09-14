9cb23bac1e446f88fcde8410d424eabd1e08c65a
zhouwenchen <761075116@qq.com>
2021-09-14 14:00:12 +0800

    剑指 Offer 10- II. 青蛙跳台阶问题

---
+++ leetcode-zwc/src/main/java/com/sh/study/offer/NumWays.java
@@ -0,0 +1,47 @@
+package com.sh.study.offer;
+
+/**
+ * 剑指 Offer 10- II. 青蛙跳台阶问题
+ * 一只青蛙一次可以跳上1级台阶，也可以跳上2级台阶。求该青蛙跳上一个 n 级的台阶总共有多少种跳法。
+ *
+ * 答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
+ *
+ * 示例 1：
+ *
+ * 输入：n = 2
+ * 输出：2
+ * 示例 2：
+ *
+ * 输入：n = 7
+ * 输出：21
+ * 示例 3：
+ *
+ * 输入：n = 0
+ * 输出：1
+ * 提示：
+ *
+ * 0 <= n <= 100
+ * 注意：本题与主站 70 题相同：https://leetcode-cn.com/problems/climbing-stairs/
+ *
+ * @author zhouwenchen
+ * @date 2021/9/13 14:44
+ **/
+public class NumWays {
+    public static int numWays(int n) {
+        if(n <= 1){
+            return 1;
+        }
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = (dp[i-1] + dp[i-2]) % 1000000007;
+        }
+
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(numWays(7));
+    }
+}
