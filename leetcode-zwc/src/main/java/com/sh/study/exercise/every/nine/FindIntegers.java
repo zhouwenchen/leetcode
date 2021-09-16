503c40e3413eed458c2569efb56bca9f0b7830c0
zhouwenchen <761075116@qq.com>
2021-09-16 13:50:08 +0800

    600. 不含连续1的非负整数

---
+++ leetcode-zwc/src/main/java/com/sh/study/exercise/every/nine/FindIntegers.java
@@ -0,0 +1,35 @@
+package com.sh.study.exercise.every.nine;
+
+/**
+ * 600. 不含连续1的非负整数
+ * 给定一个正整数 n，找出小于或等于 n 的非负整数中，其二进制表示不包含 连续的1 的个数。
+ *
+ * 示例 1:
+ *
+ * 输入: 5
+ * 输出: 5
+ * 解释:
+ * 下面是带有相应二进制表示的非负整数<= 5：
+ * 0 : 0
+ * 1 : 1
+ * 2 : 10
+ * 3 : 11
+ * 4 : 100
+ * 5 : 101
+ * 其中，只有整数3违反规则（有两个连续的1），其他5个满足规则。
+ * 说明: 1 <= n <= 109
+ *
+ * @author zhouwenchen
+ * @date 2021/9/14 16:32
+ **/
+public class FindIntegers {
+
+    public static int findIntegers(int n) {
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findIntegers(5));
+    }
+}
