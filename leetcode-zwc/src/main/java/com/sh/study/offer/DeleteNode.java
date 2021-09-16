0f826e26c63dd95ef8e3ef93b2a4dacd2315e1a9
zhouwenchen <761075116@qq.com>
2021-09-16 13:49:51 +0800

    剑指 Offer 18. 删除链表的节点

---
+++ leetcode-zwc/src/main/java/com/sh/study/offer/DeleteNode.java
@@ -0,0 +1,59 @@
+package com.sh.study.offer;
+
+import com.sh.study.node.ListNode;
+import com.sh.study.util.NodeUtil;
+
+/**
+ *
+ * 剑指 Offer 18. 删除链表的节点
+ * 给定单向链表的头指针和一个要删除的节点的值，定义一个函数删除该节点。
+ *
+ * 返回删除后的链表的头节点。
+ *
+ * 注意：此题对比原题有改动
+ *
+ * 示例 1:
+ *
+ * 输入: head = [4,5,1,9], val = 5
+ * 输出: [4,1,9]
+ * 解释: 给定你链表中值为 5 的第二个节点，那么在调用了你的函数之后，该链表应变为 4 -> 1 -> 9.
+ * 示例 2:
+ *
+ * 输入: head = [4,5,1,9], val = 1
+ * 输出: [4,5,9]
+ * 解释: 给定你链表中值为 1 的第三个节点，那么在调用了你的函数之后，该链表应变为 4 -> 5 -> 9.
+ *
+ *
+ * 说明：
+ *
+ * 题目保证链表中节点的值互不相同
+ * 若使用 C 或 C++ 语言，你不需要 free 或 delete 被删除的节点
+ *
+ * @author zhouwenchen
+ * @date 2021/9/16 9:41
+ **/
+public class DeleteNode {
+
+    public static ListNode deleteNode(ListNode head, int val) {
+        ListNode root = new ListNode(-1);
+        root.next = head;
+        ListNode pre = root;
+        ListNode cur = root.next;
+        while (cur != null){
+            if(cur.val == val){
+                pre.next = cur.next;
+            }
+            pre = cur;
+            cur = cur.next;
+        }
+
+        return root.next;
+    }
+
+    public static void main(String[] args) {
+        NodeUtil.printListNode(deleteNode(NodeUtil.createListNodeByArr(new int[]{4, 5, 1, 9}), 4));
+        NodeUtil.printListNode(deleteNode(NodeUtil.createListNodeByArr(new int[]{4, 5, 1, 9}), 5));
+        NodeUtil.printListNode(deleteNode(NodeUtil.createListNodeByArr(new int[]{4, 5, 1, 9}), 1));
+        NodeUtil.printListNode(deleteNode(NodeUtil.createListNodeByArr(new int[]{4, 5, 1, 9}), 9));
+    }
+}
