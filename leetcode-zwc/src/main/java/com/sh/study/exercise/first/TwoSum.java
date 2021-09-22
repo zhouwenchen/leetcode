6f3cd503d500217f87e0f3b28c1ca0b036d92c3b
zhouwenchen <761075116@qq.com>
2021-09-22 15:17:08 +0800

    两数之和

--- leetcode-zwc/src/main/java/com/sh/study/exercise/first/TwoSum.java
+++ leetcode-zwc/src/main/java/com/sh/study/exercise/first/TwoSum.java
@@ -98,16 +98,51 @@ public class TwoSum {
         return null;
     }
 
+    /**
+     * 剑指 Offer 57. 和为s的两个数字
+     * 输入一个递增排序的数组和一个数字s，在数组中查找两个数，使得它们的和正好是s。如果有多对数字的和等于s，则输出任意一对即可。
+     *
+     *
+     *
+     * 示例 1：
+     *
+     * 输入：nums = [2,7,11,15], target = 9
+     * 输出：[2,7] 或者 [7,2]
+     * 示例 2：
+     *
+     * 输入：nums = [10,26,30,31,47,60], target = 40
+     * 输出：[10,30] 或者 [30,10]
+     *
+     *
+     * 限制：
+     *
+     * 1 <= nums.length <= 10^5
+     * 1 <= nums[i] <= 10^6
+     * @date 2021-09-22
+     */
+    public static int[] twoSum4(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++){
+            int tmp = target - nums[i];
+            if(map.get(tmp) != null){
+                return new int[]{tmp,map.get(tmp)};
+            }
+            map.put(nums[i],tmp);
+        }
+        return null;
+    }
+
 
     public static void main(String[] args) {
-//        int[] nums = new int[]{2,7,11,15};
-//        int target = 9;
+        int[] nums = new int[]{2,7,11,15};
+        int target = 9;
 
-        int[] nums = new int[]{3,2,4};
-        int target = 6;
+//        int[] nums = new int[]{3,2,4};
+//        int target = 6;
 
 
-        int[] result = twoSum3(nums, target);
+//        int[] result = twoSum3(nums, target);
+        int[] result = twoSum4(nums, target);
         Arrays.stream(result).forEach(o-> System.out.println( o + "\t"));
     }
 }
