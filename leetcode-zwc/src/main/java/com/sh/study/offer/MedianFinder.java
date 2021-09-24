193197515557e38059589398add7cd6b05619f92
zhouwenchen <761075116@qq.com>
2021-09-24 14:16:45 +0800

    剑指 Offer 41. 数据流中的中位数

---
+++ leetcode-zwc/src/main/java/com/sh/study/offer/MedianFinder.java
@@ -0,0 +1,69 @@
+package com.sh.study.offer;
+
+import javafx.scene.layout.Priority;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/**
+ * 剑指 Offer 41. 数据流中的中位数
+ * 如何得到一个数据流中的中位数？如果从数据流中读出奇数个数值，那么中位数就是所有数值排序之后位于中间的数值。如果从数据流中读出偶数个数值，那么中位数就是所有数值排序之后中间两个数的平均值。
+ *
+ * 例如，
+ *
+ * [2,3,4] 的中位数是 3
+ *
+ * [2,3] 的中位数是 (2 + 3) / 2 = 2.5
+ *
+ * 设计一个支持以下两种操作的数据结构：
+ *
+ * void addNum(int num) - 从数据流中添加一个整数到数据结构中。
+ * double findMedian() - 返回目前所有元素的中位数。
+ * 示例 1：
+ *
+ * 输入：
+ * ["MedianFinder","addNum","addNum","findMedian","addNum","findMedian"]
+ * [[],[1],[2],[],[3],[]]
+ * 输出：[null,null,null,1.50000,null,2.00000]
+ * 示例 2：
+ *
+ * 输入：
+ * ["MedianFinder","addNum","findMedian","addNum","findMedian"]
+ * [[],[2],[],[3],[]]
+ * 输出：[null,null,2.00000,null,2.50000]
+ *
+ *
+ * 限制：
+ *
+ * 最多会对 addNum、findMedian 进行 50000 次调用。
+ * 注意：本题与主站 295 题相同：https://leetcode-cn.com/problems/find-median-from-data-stream/
+ *
+ * @author zhouwenchen
+ * @date 2021/9/23 17:49
+ **/
+public class MedianFinder {
+    Queue<Integer> A,B;
+    public MedianFinder() {
+        A = new PriorityQueue<>(); // 小顶堆，保存较大的一半
+        B = new PriorityQueue<>((a,b)->b-a); // 大顶堆
+    }
+
+    public void addNum(int num) {
+        if(A.size() != B.size()){
+            A.add(num);
+            B.add(A.poll());
+        }else {
+            B.add(num);
+            A.add(B.poll());
+        }
+    }
+
+    public double findMedian() {
+        return A.size() != B.size() ? A.peek() : (A.peek() + B.peek()) / 2.0;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
