93a2bb3c1309b0df4b43a031ee5e767c0a4c6a07
zhouwenchen <761075116@qq.com>
2021-09-24 14:16:32 +0800

    剑指 Offer 68 - I. 二叉搜索树的最近公共祖先

--- leetcode-zwc/src/main/java/com/sh/study/exercise/three/LowestCommonAncestor.java
+++ leetcode-zwc/src/main/java/com/sh/study/exercise/three/LowestCommonAncestor.java
@@ -115,6 +115,34 @@ public class LowestCommonAncestor {
      * 如果两个节点值都大于根节点，说明他们都在根节点的右子树上，我们往右子树上找
      * 如果一个节点值大于根节点，一个节点值小于根节点，说明他们他们一个在根节点的左子树上一个在根节点的右子树上，那么根节点就是他们的最近公共祖先节点。
      *
+     * 剑指 Offer 68 - I. 二叉搜索树的最近公共祖先
+     * 给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。
+     *
+     * 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+     *
+     * 例如，给定如下二叉搜索树:  root = [6,2,8,0,4,7,9,null,null,3,5]
+     *
+     *
+     *
+     *
+     *
+     * 示例 1:
+     *
+     * 输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+     * 输出: 6
+     * 解释: 节点 2 和节点 8 的最近公共祖先是 6。
+     * 示例 2:
+     *
+     * 输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+     * 输出: 2
+     * 解释: 节点 2 和节点 4 的最近公共祖先是 2, 因为根据定义最近公共祖先节点可以为节点本身。
+     *
+     *
+     * 说明:
+     *
+     * 所有节点的值都是唯一的。
+     * p、q 为不同节点且均存在于给定的二叉搜索树中。
+     *
      * @param root
      * @param p
      * @param q
@@ -148,6 +176,7 @@ public class LowestCommonAncestor {
         node1.right = node8;
         node2.left = node7;
         node2.right = node4;
+
 
 //        TreeNode root = lowestCommonAncestor(node3, node5, node1);
 //        TreeNode root = lowestCommonAncestor1(node3, node5, node1);
