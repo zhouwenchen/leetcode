d34882451b6edf6fb72b8dc24cd8ff043b637110
zhouwenchen <761075116@qq.com>
2021-09-30 15:26:58 +0800

    673. 最长递增子序列的个数

---
+++ leetcode-zwc/src/main/java/com/sh/study/exercise/every/nine/FindNumberOfLIS.java
@@ -0,0 +1,34 @@
+package com.sh.study.exercise.every.nine;
+
+/**
+ * 673. 最长递增子序列的个数
+ * 给定一个未排序的整数数组，找到最长递增子序列的个数。
+ *
+ * 示例 1:
+ *
+ * 输入: [1,3,5,4,7]
+ * 输出: 2
+ * 解释: 有两个最长递增子序列，分别是 [1, 3, 4, 7] 和[1, 3, 5, 7]。
+ * 示例 2:
+ *
+ * 输入: [2,2,2,2,2]
+ * 输出: 5
+ * 解释: 最长递增子序列的长度是1，并且存在5个子序列的长度为1，因此输出5。
+ *
+ * @author zhouwenchen
+ * @date 2021/9/21 21:22
+ **/
+public class FindNumberOfLIS {
+    /**
+     * @See com.sh.study.exercise.eight.LengthOfLIS
+     * @param nums
+     * @return
+     */
+    public static int findNumberOfLIS(int[] nums) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
