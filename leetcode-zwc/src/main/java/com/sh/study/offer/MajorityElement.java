4ed058877f5ec03f7f3c75981d53af2fabdc1281
zhouwenchen <761075116@qq.com>
2021-10-22 11:42:44 +0800

    剑指 Offer 39. 数组中出现次数超过一半的数字

--- leetcode-zwc/src/main/java/com/sh/study/offer/MajorityElement.java
+++ leetcode-zwc/src/main/java/com/sh/study/offer/MajorityElement.java
@@ -1,6 +1,8 @@
 package com.sh.study.offer;
 
-import java.util.Arrays;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * 剑指 Offer 39. 数组中出现次数超过一半的数字
@@ -59,8 +61,78 @@ public class MajorityElement {
         return candidate;
     }
 
+
+    /**
+     * 229. 求众数 II
+     * 给定一个大小为 n 的整数数组，找出其中所有出现超过 ⌊ n/3 ⌋ 次的元素。
+     *
+     *
+     *
+     *
+     *
+     * 示例 1：
+     *
+     * 输入：[3,2,3]
+     * 输出：[3]
+     * 示例 2：
+     *
+     * 输入：nums = [1]
+     * 输出：[1]
+     * 示例 3：
+     *
+     * 输入：[1,1,1,3,3,2,2,2]
+     * 输出：[1,2]
+     *
+     *
+     * 提示：
+     *
+     * 1 <= nums.length <= 5 * 104
+     * -109 <= nums[i] <= 109
+     * @param nums
+     * @return
+     */
+    public static List<Integer> majorityElement2(int[] nums) {
+        // 统计出现所有元素的个数
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++){
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+        List<Integer> list = new ArrayList<>();
+        for (Integer key: map.keySet()){
+            if(map.get(key) > nums.length / 3){
+                list.add(key);
+            }
+        }
+        return list;
+    }
+
+    public static List<Integer> majorityElement3(int[] nums) {
+        int max = nums.length / 3;
+        return Arrays.stream(nums).boxed()
+                .collect(Collectors.groupingBy(Function.identity(),Collectors.counting()))
+                .entrySet()
+                .stream()
+                .filter(e->e.getValue() > max)
+                .map(e->e.getKey())
+                .collect(Collectors.toList());
+    }
+
+    public static List<Integer> majorityElement4(int[] nums) {
+        return Arrays.stream(nums)
+                .boxed()
+                .collect(Collectors.toMap(k->k,v->1,Integer::sum))
+                .entrySet()
+                .stream()
+                .filter(o->o.getValue() > nums.length / 3)
+                .map(o->o.getKey())
+                .collect(Collectors.toList());
+    }
+
     public static void main(String[] args) {
-        System.out.println(majorityElement(new int[]{1, 2, 3, 2, 2, 2, 5, 4, 2}));
-        System.out.println(majorityElement1(new int[]{1, 2, 3, 2, 2, 2, 5, 4, 2}));
+//        System.out.println(majorityElement(new int[]{1, 2, 3, 2, 2, 2, 5, 4, 2}));
+//        System.out.println(majorityElement1(new int[]{1, 2, 3, 2, 2, 2, 5, 4, 2}));
+
+//        majorityElement2(new int[]{1,1,1,3,3,2,2,2}).stream().forEach(o-> System.out.print(o+"\t"));
+        majorityElement3(new int[]{1,1,1,3,3,2,2,2}).stream().forEach(o-> System.out.print(o+"\t"));
     }
 }
